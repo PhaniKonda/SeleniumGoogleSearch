5e1784b8a78044a6b701b7ee149ba06f9fb2a97a
Phani KOnda <govardan.phani@gmail.com>
2021-02-12 22:43:50 +0000

    Updations and added runner

--- src/main/java/com/google/WebDriverInstance.java
+++ src/main/java/com/google/WebDriverInstance.java
@@ -2,10 +2,12 @@ package com.google;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
-import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class WebDriverInstance {
@@ -13,13 +15,16 @@ public class WebDriverInstance {
     @BeforeSuite
 
     public WebDriver getDriver(String browser){
-        if(browser.toLowerCase().equals("chrome")){
-            System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
-            WebDriver driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-            driver.get("http://google.com");
-            return driver;
+        WebDriver driver;
+        switch(browser.toLowerCase()){
+            case "chrome":
+            case "google chrome":
+                System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+                driver = new ChromeDriver();
+                driver.manage().window().maximize();
+                driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+                driver.get("http://google.com");
+                return driver;
         }
         return null;
     }
